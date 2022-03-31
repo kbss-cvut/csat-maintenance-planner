6c807a059112b6ebd85e516053612d07457c2c45
Max Chopart <max.chopart@gmail.com>
2022-03-31 11:46:43 +0200

    [Upd] Remove useless CRA assets

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import WorkPackage from './WorkPackage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WorkPackage />
   </React.StrictMode>,
   document.getElementById('root')
 );
