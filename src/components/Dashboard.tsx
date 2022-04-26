667046094a94370b5dffe7c6f48ede56cf73368a
Max Chopart <max.chopart@gmail.com>
2022-04-26 11:54:32 +0200

    [New] Add react planning tool

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,27 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import moment from "moment";
 import Constants from "../utils/Constants";
-import {
-  RevisionPlanInterface,
-  WorkPackageInterface,
-} from "../utils/Interfaces";
+import { WorkPackageInterface } from "../utils/Interfaces";
 
 import styles from "./Dashboard.module.scss";
 import LoadingSpinnerIcon from "../styles/icons/LoadindSpinnerIcon";
 import RevisionPlanList from "./RevisionPlanList";
-import RevisionPlan from "./RevisionPlan";
 import WorkPackageList from "./WorkPackageList";
 import PlanningTool from "planning-tool";
-
 import "planning-tool/dist/PlanningTool.css";
+import data from "./data3.json";
 
 const Dashboard = () => {
   const [workPackageList, setWorkPackageList] = useState<
     Array<WorkPackageInterface>
   >([]);
   const [revisionPlanList, setRevisionPlanList] = useState<Array<string>>([]);
-  const [revisionPlanData, setRevisionPlanData] =
-    useState<RevisionPlanInterface | null>(null);
+  const [revisionPlanData, setRevisionPlanData] = useState<Array<any> | null>(
+    data
+  );
 
   const [isListLoading, setIsListLoading] = useState<boolean>(false);
   const [isRevisionLoading, setIsRevisionLoading] = useState<boolean>(false);
@@ -74,9 +72,15 @@ const Dashboard = () => {
     setRevisionErrorMessage("");
     const fetchRevisionPlanData = async () => {
       const revisionTitle = revisionPlanList[index];
-      const revisionId = revisionTitle.replace(" ", "%20").split(",")[0];
+      const revisionId = revisionTitle
+        .replaceAll(" ", "%20")
+        .replaceAll("/", "%2F")
+        .replaceAll("+", "%2B")
+        .split(",")[0];
       const { data } = await axios.get(Constants.REVISION_ID + revisionId);
-      setRevisionPlanData(data);
+      let planData: any[] = [];
+      planData.push(data);
+      setRevisionPlanData(planData);
     };
     fetchRevisionPlanData().then(() => {
       setIsRevisionLoading(false);
@@ -118,17 +122,107 @@ const Dashboard = () => {
     );
   };
 
-  const renderRevisionPlan = () => {
+  const renderPlanningTool = () => {
     return (
       <React.Fragment>
         {revisionErrorMessage && <p>{revisionErrorMessage}</p>}
         {isRevisionLoading && !revisionErrorMessage && <LoadingSpinnerIcon />}
         {!isRevisionLoading && revisionPlanData && !revisionErrorMessage && (
-          <RevisionPlan revisionPlanData={revisionPlanData} />
+          <PlanningTool items={items} groups={groups} />
         )}
       </React.Fragment>
     );
   };
+
+  const items = [];
+  const groupsMap = new Map();
+
+  const getTaskBackground = (task) => {
+    if (!task["task-type"]) {
+      return "#2196F3";
+    }
+    switch (task["task-type"]["task-category"]) {
+      case "scheduled_wo":
+        return "#aa0000";
+      case "task_card":
+        return "#00aa00";
+      case "maintenance_wo":
+        return "#0000aa";
+      default:
+        return "#2196F3";
+    }
+  };
+
+  const buildData = (data, groupsMap, items, level, groupParentId) => {
+    if (!data || !Array.isArray(data)) {
+      return;
+    }
+
+    for (const item of data) {
+      const resourceId = item.resource.id + " - " + item.resource.type;
+      if (!groupsMap.has(resourceId)) {
+        groupsMap.set(resourceId, {
+          id: groupsMap.size,
+          title: item.resource ? item.resource.title : "",
+          hasChildren: item.planParts && item.planParts.length > 0,
+          parent: groupParentId,
+          open: level < 1,
+          show: level < 2,
+          level: level,
+        });
+      }
+
+      const date = moment(
+        item.type === "SessionPlan"
+          ? item["start-time"]
+          : item["planned-start-time"]
+      )
+        .add("1", "year")
+        .add("2", "month")
+        .add("27", "day");
+      const endDate = moment(
+        item.type === "SessionPlan"
+          ? item["end-time"]
+          : item["planned-end-time"]
+      )
+        .add("1", "year")
+        .add("2", "month")
+        .add("27", "day");
+      const itemId = items.length + 1;
+
+      items.push({
+        id: itemId,
+        group: groupsMap.get(resourceId).id,
+        title: item.title,
+        start: date,
+        end: endDate,
+        className: "item",
+        bgColor: getTaskBackground(item),
+        color: "#fff",
+        selectedBgColor: "#FFC107",
+        selectedColor: "#000",
+        draggingBgColor: "#f00",
+        highlightBgColor: "#FFA500",
+        highlight: false,
+        canMove: level > 1 && level !== 3,
+        canResize: "both", //'left','right','both', false
+        minimumDuration: 120, //minutes
+      });
+
+      if (item.planParts && item.planParts.length > 0) {
+        buildData(
+          item.planParts,
+          groupsMap,
+          items,
+          level + 1,
+          groupsMap.get(resourceId).id
+        );
+      }
+    }
+  };
+
+  buildData(revisionPlanData, groupsMap, items, 0, null);
+  const groups = Array.from(groupsMap, ([key, values]) => values);
 
   return (
     <div className={styles.container}>
@@ -150,9 +244,7 @@ const Dashboard = () => {
           Update
         </button>
       </div>
-      <div className={styles.planning}>
-        <PlanningTool />
-      </div>
+      <div className={styles.planning}>{renderPlanningTool()}</div>
     </div>
   );
 };
