666365255fa3cba50e57665675d81f46dac623a8
Max Chopart <max.chopart@gmail.com>
2022-05-16 17:33:29 +0200

    [New] Add environment variables at runtime for dockerisation

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,10 +1,45 @@
+/**
+ * Aggregated object of process.env and window.__config__ to allow dynamic configuration
+ */
+export const ENV = {
+  ...Object.keys(process.env).reduce<Record<string, string>>((acc, key) => {
+    const strippedKey = key.replace("REACT_APP_", "");
+    acc[strippedKey] = process.env[key]!;
+    return acc;
+  }, {}),
+  ...(window as any).__config__,
+};
+
+/**
+ * Helper to make sure that all envs are defined properly
+ * @param name env variable name (without the REACT_APP prefix)
+ * @param defaultValue Default variable name
+ */
+export function getEnv(name: string, defaultValue?: string): string {
+  const value = ENV[name] || defaultValue;
+  if (value !== undefined) {
+    return value;
+  }
+  throw new Error(`Missing environment variable: ${name}`);
+}
+
 const Constants = {
-  API: "/api",
-  CSAT_PLANNING_URL: "https://kbss.felk.cvut.cz/csat",
-  CSAT_PLANNING_WORKPACKAGES_LIST_URL: "/workpackages/list",
-  CSAT_PLANNING_WORKPACKAGE_DASHBOARD: "/dashboard.html?wp=",
-  REVISION_LIST: "https://kbss.felk.cvut.cz/csat/api2/revisions/",
-  REVISION_ID: "https://kbss.felk.cvut.cz/csat/api2/plan-revision2?revisionId=",
+  SERVER_URL_WORKPACKAGE_LIST: getEnv(
+    "SERVER_URL_WORKPACKAGE_LIST",
+    "https://kbss.felk.cvut.cz/csat/api/workpackages/list"
+  ),
+  SERVER_URL_WORKPACKAGE_DASHBOARD: getEnv(
+    "SERVER_URL_WORKPACKAGE_DASHBOARD",
+    "https://kbss.felk.cvut.cz/csat/dashboard.html?wp="
+  ),
+  SERVER_URL_REVISION_LIST: getEnv(
+    "SERVER_URL_REVISION_LIST",
+    "https://kbss.felk.cvut.cz/csat/api2/revisions/"
+  ),
+  SERVER_URL_REVISION_ID: getEnv(
+    "SERVER_URL_REVISION_ID",
+    "https://kbss.felk.cvut.cz/csat/api2/revision-plans-induced-by-revision-execution?revisionId="
+  ),
 };
 
 export default Constants;
